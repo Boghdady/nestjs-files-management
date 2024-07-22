7bececd2f6626f266c8dac6ed9639bfa58789141
boghdady <boghdady107@gmail.com>
2024-07-22 15:44:27 +0300

    3-Use built-in parse file pipe to validate multiple files

--- src/files-upload/files-upload.controller.ts
+++ src/files-upload/files-upload.controller.ts
@@ -52,14 +52,35 @@ export class FilesUploadController {
   }
 
   @Post('/multiple')
-  @UseInterceptors(
-    FilesInterceptor('files', 3, {
-      limits: {
-        fileSize: 2 * 1024 * 1024, // 2MB
-      },
-    }),
-  )
-  uploadFiles(@UploadedFiles() files: File[]) {
+  @UseInterceptors(FilesInterceptor('files', 3))
+  uploadFiles(
+    @UploadedFiles(
+      new ParseFilePipe({
+        validators: [
+          // 1) validate file size
+          new MaxFileSizeValidator({
+            maxSize: 2 * 1024 * 1024, // 2MB
+            message: (maxSize) =>
+              `File is too big. Max file size is ${maxSize} bytes`,
+          }),
+
+          // 2) validate file type (extension)
+          new FileTypeValidator({
+            fileType: /png|jpg|pdf/,
+          }),
+
+          // 3) custom validation
+        ],
+        errorHttpStatusCode: HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+        exceptionFactory: (error: string) => {
+          console.log('error', error);
+          throw new UnprocessableEntityException(error);
+        },
+        fileIsRequired: true,
+      }),
+    )
+    files: File[],
+  ) {
     // this.awsS3.uploadMultipleFiles(files);
     return files.map((file) => file.originalname);
   }
