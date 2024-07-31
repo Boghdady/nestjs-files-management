3aa35bef02cf29b4765cc69ccfead6ae51619608
boghdady <boghdady107@gmail.com>
2024-07-31 16:48:39 +0300

    6-Refactoring Part-2

--- src/shared/files/files-validation-factory.ts
+++ src/shared/files/files-validation-factory.ts
@@ -6,31 +6,35 @@ import {
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { FileSignatureValidator } from './validators/file-signature.validator';
+import { FileValidator } from '@nestjs/common/pipes/file/file-validator.interface';
+
+const createFileValidators = (
+  maxSize: number,
+  fileType: RegExp | string,
+): FileValidator[] => [
+  // 1) validate file size
+  new MaxFileSizeValidator({
+    maxSize, // 2MB
+    message: (maxSize) => `File is too big. Max file size is ${maxSize} bytes`,
+  }),
+
+  // 2) validate file type (extension)
+  new FileTypeValidator({
+    fileType,
+  }),
+
+  // 3) custom validation (validate file signature)
+  new FileSignatureValidator(),
+];
 
 export const createParseFilePipe = (
   maxSize: number,
   fileType: RegExp | string,
 ): ParseFilePipe =>
   new ParseFilePipe({
-    validators: [
-      // 1) validate file size
-      new MaxFileSizeValidator({
-        maxSize, // 2MB
-        message: (maxSize) =>
-          `File is too big. Max file size is ${maxSize} bytes`,
-      }),
-
-      // 2) validate file type (extension)
-      new FileTypeValidator({
-        fileType,
-      }),
-
-      // 3) custom validation (validate file signature)
-      new FileSignatureValidator(),
-    ],
+    validators: createFileValidators(maxSize, fileType),
     errorHttpStatusCode: HttpStatus.UNSUPPORTED_MEDIA_TYPE,
     exceptionFactory: (error: string) => {
-      console.log('error', error);
       throw new UnprocessableEntityException(error);
     },
     fileIsRequired: true,
